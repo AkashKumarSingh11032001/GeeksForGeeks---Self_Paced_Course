9edb077a47ea6f5b141153e6799bc0d9a46db21a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-15 18:33:25 +0530

    factorialDigit

--- 2. Mathematics/Problem's/factorialDigit.cpp
+++ 2. Mathematics/Problem's/factorialDigit.cpp
@@ -4,28 +4,28 @@
 
 using namespace std;
 
-int fact(int x){
+int fact(int x)
+{
     if (x == 0)
         return 1;
-    return x* fact(x-1);
+    return x * fact(x - 1);
 }
 int factDig(int res)
 {
-    
+
     int count = 0;
     while (res != 0)
     {
         /* code */
         count++;
-        res = res /10;
+        res = res / 10;
     }
     return count;
-    
 }
 int main()
 {
     int a = fact(12);
-    cout << factDig(a); 
-    
+    cout << factDig(a);
+
     return 0;
 }
