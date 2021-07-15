5bb589449454d34f60a2d0b9ef69fdf4d2cabd8f
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-15 18:19:47 +0530

    quadaticRoot

--- 2. Mathematics/Problem's/quadaticRoot.cpp
+++ 2. Mathematics/Problem's/quadaticRoot.cpp
@@ -4,16 +4,17 @@
 
 using namespace std;
 
-void root(int a, int b, int c){
-    int dis = pow(b,2) - 4*a*c;
-    int root1 = (-b + sqrt(dis)) / 2*a;
-    int root2 = (-b - sqrt(dis)) / 2*a;
+void root(int a, int b, int c)
+{
+    int dis = pow(b, 2) - 4 * a * c;
+    int root1 = (-b + sqrt(dis)) / 2 * a;
+    int root2 = (-b - sqrt(dis)) / 2 * a;
 
-    cout << root1 <<" " << root2;
+    cout << root1 << " " << root2;
 }
 int main()
 {
-    
-    root(1,-2,1);
+
+    root(1, -2, 1);
     return 0;
 }
