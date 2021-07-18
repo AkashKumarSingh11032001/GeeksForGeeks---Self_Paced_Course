29366b9aa931c032a39891979917219779cb0100
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-18 17:15:42 +0530

    seiveOfErathemis

--- 2. Mathematics/seiveOfErathemis.cpp
+++ 2. Mathematics/seiveOfErathemis.cpp
@@ -4,23 +4,22 @@
 
 using namespace std;
 
-
-void seiveOfErathemis(int n){
-    vector <bool> checkPrime(n+1,true);
+void seiveOfErathemis(int n)
+{
+    vector<bool> checkPrime(n + 1, true);
     for (int i = 2; i <= n; i++)
     {
         /* code */
-        if(checkPrime[i]){
-            cout << i<<" ";
-            for (int j = i*i; j <= n; j=j+i)
+        if (checkPrime[i])
+        {
+            cout << i << " ";
+            for (int j = i * i; j <= n; j = j + i)
             {
                 /* code */
                 checkPrime[j] = false;
             }
-            
         }
     }
-    
 }
 int main()
 {
