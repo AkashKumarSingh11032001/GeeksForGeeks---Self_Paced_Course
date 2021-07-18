434343d0fecb781a27f6d79df887cdfba956cf4d
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-18 16:52:59 +0530

    primeFactor

---
+++ 2. Mathematics/primeFactor.cpp
@@ -0,0 +1,44 @@
+// primeFactor.cpp
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void primeFactor(int n){
+    if(n <= 1){
+        return;
+    }
+    while( n%2 == 0){
+        cout << 2<< " ";
+        n = n/2;
+    }
+    while (n%3 == 0)
+    {
+        /* code */
+        cout<< 3<< " ";
+        n = n/3;
+    }
+    for (int i = 5; i*i <= n; i = i+6)
+    {
+        /* code */
+        while(n%i == 0){
+            cout << i << " ";
+            n=n/i;
+        }
+        while(n%(i+2) == 0){
+            cout << i+2<< " ";
+            n = n/(i+2);
+        }
+    }
+    if(n>3){
+        cout<< n<< " ";
+    }
+    
+    
+}
+int main()
+{
+    primeFactor(25);
+    return 0;
+}
