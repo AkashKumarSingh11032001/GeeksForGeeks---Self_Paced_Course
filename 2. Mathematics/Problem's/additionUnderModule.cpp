bebeebc3a4137f73b90b50fb830d01bc0c045482
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-19 13:36:50 +0530

    additionUnderModule

--- 2. Mathematics/Problem's/additionUnderModule.cpp
+++ 2. Mathematics/Problem's/additionUnderModule.cpp
@@ -4,14 +4,15 @@
 using namespace std;
 
 // ---> (a+b)%m = (a%m+b%m) % m [properties]
-long long int mod(long long int a, long long int b){
+long long int mod(long long int a, long long int b)
+{
     const unsigned int M = 1000000007;
 
-    long long int sum = (a%M + b%M) % M;
+    long long int sum = (a % M + b % M) % M;
     return sum;
 }
 int main()
 {
-    cout << mod(9223372036854775807,9223372036854775807);
+    cout << mod(9223372036854775807, 9223372036854775807);
     return 0;
 }
