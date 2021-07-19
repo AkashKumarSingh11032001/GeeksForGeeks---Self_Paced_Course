cec065c1e9de61282743625cc86c9df43fa178ba
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-19 14:06:13 +0530

    modulorMulticativeInverse

---
+++ 2. Mathematics/Problem's/modulorMulticativeInverse.cpp
@@ -0,0 +1,34 @@
+// modulorMulticativeInverse.cpp
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// int findgcd(int a, int m){
+//     if (m==0){
+//         return a;
+//     }
+//     else{
+//         return findgcd(m,a%m);
+//     }
+// }
+int inverse(int a, int m){
+
+    
+        for (int i = 1; i < m; ++i)
+        {
+            /* code */
+            if (((a%m) * (i%m)%m) == 1)
+                return i;
+        }
+        
+    
+
+
+}
+
+int main()
+{
+    cout << inverse(3,11);
+    return 0;
+}
