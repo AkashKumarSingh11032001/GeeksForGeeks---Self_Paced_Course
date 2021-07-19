910904492806d120988800bc17db5c14de6ea5fe
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-19 09:39:52 +0530

    isPrime

--- 2. Mathematics/Problem's/isPrime.cpp
+++ 2. Mathematics/Problem's/isPrime.cpp
@@ -3,24 +3,27 @@
 
 using namespace std;
 
-bool isPrime(int n){
-    if (n == 1){
+bool isPrime(int n)
+{
+    if (n == 1)
+    {
         return false;
     }
-    if( n==2 || n==3){
+    if (n == 2 || n == 3)
+    {
         return true;
     }
-    if(n%2 == 0 || n%3 == 0){
+    if (n % 2 == 0 || n % 3 == 0)
+    {
         return false;
     }
-    for (int i = 5; i*i <= n; i = i+6)
+    for (int i = 5; i * i <= n; i = i + 6)
     {
         /* code */
-        if(n%i == 0 || n%(i+2) == 0)
+        if (n % i == 0 || n % (i + 2) == 0)
             return false;
     }
     return true;
-    
 }
 int main()
 {
