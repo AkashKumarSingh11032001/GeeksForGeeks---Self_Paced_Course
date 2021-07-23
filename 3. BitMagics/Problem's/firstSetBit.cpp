74429c64a169b02f9315a138eabb253572d9bc1b
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-23 21:08:37 +0530

    firstSetBit

---
+++ 3. BitMagics/Problem's/firstSetBit.cpp
@@ -0,0 +1,24 @@
+// firstSetBit.cpp
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int firstSetBit(int n){
+    int mask = 1;
+    int counter = 1;
+
+    while (n)
+    {
+        /* code */
+        if(n & mask)
+            return counter;
+        n = n >> 1;
+        counter++;
+    }
+    
+}
+int main()
+{
+    cout << firstSetBit(12);
+    return 0;
+}
