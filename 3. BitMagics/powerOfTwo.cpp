b1043d651a1b724e7d9be5b6a1b81a23c063b195
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-23 08:48:21 +0530

    powerOfTwo

--- 3. BitMagics/powerOfTwo.cpp
+++ 3. BitMagics/powerOfTwo.cpp
@@ -5,31 +5,32 @@
 
 using namespace std;
 
-bool naive(int n){
+bool naive(int n)
+{
     if (n == 0)
         return false;
     while (n != 1)
     {
         /* code */
-        if(n % 2 != 0)
+        if (n % 2 != 0)
             return false;
         n = n / 2;
     }
     return true;
-    
 }
 
-bool bestSol(int n){
-    if(n == 0)
+bool bestSol(int n)
+{
+    if (n == 0)
         return false;
-    return ((n & (n-1)) == 0);
+    return ((n & (n - 1)) == 0);
 }
 int main()
 {
-    
-    cout << naive(4) <<endl;
-    cout << "*****" <<endl;
-    cout << bestSol(4) <<endl;
+
+    cout << naive(4) << endl;
+    cout << "*****" << endl;
+    cout << bestSol(4) << endl;
 
     return 0;
 }
