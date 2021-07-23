a0895b1dac99ce70439de3ea9ee5f95e5f283a4a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-23 07:56:13 +0530

    shiftBitwise

--- 3. BitMagics/shiftBitwise.cpp
+++ 3. BitMagics/shiftBitwise.cpp
@@ -3,28 +3,28 @@
 
 using namespace std;
 
-void leftShiftBitwise(int x){
+void leftShiftBitwise(int x)
+{
 
-    cout << (x << 1) <<endl; // ---> Left Shift 1 bit
-    cout << (x << 2) <<endl; // ---> Left Shift 2 bit
+    cout << (x << 1) << endl; // ---> Left Shift 1 bit
+    cout << (x << 2) << endl; // ---> Left Shift 2 bit
     int y = 4;
     int z = (x << y); // ---> Left Shift y bit
-    cout << z <<endl;
-
+    cout << z << endl;
 }
-void rightShiftBitwise(int x){
+void rightShiftBitwise(int x)
+{
 
-    cout << (x >> 1) <<endl; // ---> right Shift 1 bit
-    cout << (x >> 2) <<endl; // ---> right Shift 2 bit
+    cout << (x >> 1) << endl; // ---> right Shift 1 bit
+    cout << (x >> 2) << endl; // ---> right Shift 2 bit
     int y = 4;
     int z = (x >> y); // ---> right Shift y bit
-    cout << z <<endl;
-
+    cout << z << endl;
 }
 int main()
 {
     leftShiftBitwise(3);
-    cout << "******" <<endl;
+    cout << "******" << endl;
     rightShiftBitwise(3);
     return 0;
 }
