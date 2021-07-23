ba1d219e316a830404e44b135e872d9ec70b331d
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-23 08:23:04 +0530

    multiplicationAndDivision

--- 3. BitMagics/multiplicationAndDivision.cpp
+++ 3. BitMagics/multiplicationAndDivision.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main()
 {
-    int x = 18,y = 2;
-    cout << (x << 1) <<endl; // ---> multiple by 2, 1 time [18 * 2]
-    cout << (x << 2) <<endl;  // ---> multiple by 2, 2 time [18 * 2 * 2]
-    cout << (x << 3) <<endl;  // ---> multiple by 2, 3 time [18 * 2 * 2 * 2]
-    cout << "****" <<endl; 
-    cout << (x >> 1) <<endl;  // ---> div by 2, 1 time [18 / 2]
-    cout << (x >> 2) <<endl;  // ---> div by 2, 2 time [18 / 2 * 2 ]
+    int x = 18, y = 2;
+    cout << (x << 1) << endl; // ---> multiple by 2, 1 time [18 * 2]
+    cout << (x << 2) << endl; // ---> multiple by 2, 2 time [18 * 2 * 2]
+    cout << (x << 3) << endl; // ---> multiple by 2, 3 time [18 * 2 * 2 * 2]
+    cout << "****" << endl;
+    cout << (x >> 1) << endl; // ---> div by 2, 1 time [18 / 2]
+    cout << (x >> 2) << endl; // ---> div by 2, 2 time [18 / 2 * 2 ]
     return 0;
 }
