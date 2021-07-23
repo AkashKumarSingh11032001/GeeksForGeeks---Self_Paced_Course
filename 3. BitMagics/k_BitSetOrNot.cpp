86784cad37c2123ce4137527b261460221351bad
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-23 08:16:36 +0530

    k_BitSetOrNot

---
+++ 3. BitMagics/k_BitSetOrNot.cpp
@@ -0,0 +1,18 @@
+// k_BitSetOrNot.cpp
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// ---> Checking if bit at k-th position is set or unset ?
+void kBit(int n, int k){
+
+    if( (n & (1 << (k-1) ) != 0))
+        cout << "Yes";
+    else
+        cout << "No";
+}
+int main()
+{
+    kBit(16,3);
+    return 0;
+}
