b2608a132cf3e134507b6e5e30137338446c1188
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-23 08:02:53 +0530

    bitwiseNOT

--- 3. BitMagics/bitwiseNOT.cpp
+++ 3. BitMagics/bitwiseNOT.cpp
@@ -3,21 +3,24 @@
 
 using namespace std;
 
-void unsigned_bitwiseNOT(unsigned int x){
-    cout << (~x) <<endl;
+void unsigned_bitwiseNOT(unsigned int x)
+{
+    cout << (~x) << endl;
     x = 5;
-    cout << (~x) <<endl;
+    cout << (~x) << endl;
 }
 
-void signed_bitwiseNOT(int x){
-    cout << (~x) <<endl;
+void signed_bitwiseNOT(int x)
+{
+    cout << (~x) << endl;
     x = 5;
-    cout << (~x) <<endl;
+    cout << (~x) << endl;
 }
+
 int main()
 {
     unsigned_bitwiseNOT(5);
-    cout << "*****" <<endl;
+    cout << "*****" << endl;
     signed_bitwiseNOT(5);
     return 0;
 }
