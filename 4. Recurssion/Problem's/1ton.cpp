c0791e142a1701015dc277c3cbf292b8e1a20353
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-29 13:06:32 +0530

    1ton

---
+++ 4. Recurssion/Problem's/1ton.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int printnum(int x){
+    if(x == 0)
+        return x;
+    printnum(x-1);
+    cout << x << " ";
+}
+int main()
+{
+    printnum(5);
+    return 0;
+}
