4f27d074c2e84c6dd730b8d496234547a74244d0
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-29 13:26:28 +0530

    digitCount

---
+++ 4. Recurssion/Problem's/digitCount.cpp
@@ -0,0 +1,25 @@
+// digitCount.cpp
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int digitCount(int n,int count){
+
+    if( n == 0)
+        return count;
+
+    int lastDig = n%10;
+    count++;
+
+    return (digitCount(n/10,count));
+
+    
+}
+int main()
+{
+    int count = 0;
+    cout << digitCount(52874,count);
+    return 0;
+}
