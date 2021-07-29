64dd6a23514eebdc0f43fbc5251082c60cf9fa1e
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-29 14:18:04 +0530

    isPalidrom

---
+++ 4. Recurssion/Problem's/isPalidrom.cpp
@@ -0,0 +1,18 @@
+// isPalidrom.cpp
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+bool isPalidrom(string &str,int start, int end){
+    if(start >= end)
+        return true;
+    return (str[start] == str[end] && isPalidrom(str,start+1,end - 1));
+}
+int main()
+{
+    string name = "aba";
+    cout << isPalidrom(name,0,name.length());
+    return 0;
+}
