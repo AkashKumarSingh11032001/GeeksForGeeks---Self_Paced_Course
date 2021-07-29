d7b479e08cb8f818d4252b230471a299a81a47af
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-29 14:36:31 +0530

    ropeCuttinggProblem

--- 4. Recurssion/ropeCuttinggProblem.cpp
+++ 4. Recurssion/ropeCuttinggProblem.cpp
@@ -4,20 +4,21 @@
 
 using namespace std;
 
-int ropeCuttinggProblem(int n, int a, int b, int c){
+int ropeCuttinggProblem(int n, int a, int b, int c)
+{
     if (n == 0)
         return 0;
     if (n < 0)
         return -1;
 
-    int res = max(ropeCuttinggProblem(n-a,a,b,c),ropeCuttinggProblem(n-b,a,b,c),ropeCuttinggProblem(n-c,a,b,c));
-    
+    int res = max(ropeCuttinggProblem(n - a, a, b, c), ropeCuttinggProblem(n - b, a, b, c), ropeCuttinggProblem(n - c, a, b, c));
+
     if (res == -1)
         return -1;
-    return res+1;
+    return res + 1;
 }
 int main()
 {
-    ropeCuttinggProblem(23,11,9,12);
+    ropeCuttinggProblem(23, 11, 9, 12);
     return 0;
 }
