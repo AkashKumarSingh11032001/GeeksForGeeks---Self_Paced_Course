114b34616a9d80dc7ce21ba73fb41c38e3ee586b
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-07-29 13:30:55 +0530

    digitalRoot

---
+++ 4. Recurssion/Problem's/digitalRoot.cpp
@@ -0,0 +1,18 @@
+// digitalRoot.cpp
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int digitalRoot(int n){
+    if (n == 0)
+        return n;
+
+    return (n%10 + digitalRoot(n/10));
+}
+int main()
+{
+    cout << digitalRoot(digitalRoot(5));
+    return 0;
+}
