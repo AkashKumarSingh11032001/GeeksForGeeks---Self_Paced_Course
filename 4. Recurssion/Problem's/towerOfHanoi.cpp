f3f56c086f77f29a9d17062bfa9ac85bcbc82ef4
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-08-06 08:59:57 +0530

    code

---
+++ 4. Recurssion/Problem's/towerOfHanoi.cpp
@@ -0,0 +1,24 @@
+// towerOfHanoi.cpp
+// A---> source
+// B---> auxilary
+// C---> destination
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void TOH(int n,int A, int B,int C){
+
+    if(n==1){
+        cout << "move from poll" << A << "to" << C <<endl; 
+    }
+
+    TOH(n-1,A,C,B);
+    cout << "move from poll" << A << "to" << C << "Using" << B <<  endl;
+    TOH(n-1,B,A,C); 
+} 
+
+int main()
+{
+    TOH(3,1, 2, 3);
+    return 0;
+}
