5b8193769db5aa6174883108ba2fd0411744da3d
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-01-18 15:53:28 +0530

    CP

--- 6. Hashing/SubArraysWithGivenSum.cpp
+++ 6. Hashing/SubArraysWithGivenSum.cpp
@@ -1,24 +1,26 @@
 // Pair vs Subarray
 // pair can be non-contiguous while subarray is contiguous
 
-#include <bits/stdc++.h>
+
+#include<bits/stdc++.h>
 using namespace std;
-int PairSum(int a[],int n,int sum){
+
+bool Subarray(int a[],int n,int sum){
 
     unordered_set<int>s;
+    int pre_sum = 0;
     for(int i = 0; i <n; i++){
-        if(s.find(sum-a[i]) != s.end()){
-            return true;
-        }
+        pre_sum += a[i];
+        if(pre_sum == sum) return true;
+        if(s.find(pre_sum - sum) != s.end()) return true;
         s.insert(a[i]);
     }
-    return false;
 }
 
-int main() {
+int main(){
 
     int a[] = {1,2,3,4};
     int sum = 6;
     int n =sizeof(a)/sizeof(a[0]);
-    cout << PairSum(a,n,sum) << endl;
+    cout << Subarray(a,n,sum) << endl;
 }
